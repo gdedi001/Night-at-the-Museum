f9b75d2089d243e0f77d820915d85835f55317ef
Gabriel de Diego <gdedi651@gmail.com>
2017-05-09 01:35:00 -0400

    feat: add waypoint locamotion
    Obtain prefab from 'MovementExamples' - waypoint locamotion was selected

---
+++ Assets/Scripts/WaypointMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaypointMovement : MonoBehaviour {
+	
+	public GameObject player;
+
+
+	public float height = 2;
+	public bool teleport = true;
+
+	public float maxMoveDistance = 10;
+	private bool moving = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Move(GameObject waypoint) {
+		if (!teleport) {
+			iTween.MoveTo (player, 
+				iTween.Hash (
+					"position", new Vector3 (waypoint.GetComponent<Transform> ().position.x, waypoint.GetComponent<Transform> ().position.y + height / 2, waypoint.GetComponent<Transform> ().position.z), 
+					"time", .2F, 
+					"easetype", "linear"
+				)
+			);
+		} else {
+			player.transform.position = new Vector3 (waypoint.GetComponent<Transform> ().position.x, waypoint.GetComponent<Transform> ().position.y + height / 2, waypoint.GetComponent<Transform> ().position.z);
+		}
+	}
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Night-at-the-Museum
 VR Museum Experience for the Udacity Nanodegree Program
+
+## Things I Learned
+- Exporting prefabs/assets from old projects
+- Submitting application to Google's Play Store
